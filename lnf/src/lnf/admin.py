0444fd36a11ec8c04f4580589a83a1655b97466e
mephi007 <roysumit949@gmail.com>
2018-09-17 20:25:41 +0530

    successfully table is created in admin page

--- lnf/src/lnf/admin.py
+++ lnf/src/lnf/admin.py
@@ -3,7 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from lnf.models import Admin_Page
 
+class PostModelAdmin(admin.ModelAdmin):
+	list_display = ['Name', 'Contact', 'Lost_Found_Product', 'Small_Description','Tags','Location','Date_and_Time']
+
+	# list_filter = ["updated", "timestamp"]
+	# search_fields = ["title"
+
+
+	class Meta:
+		model = Admin_Page
+
+
+admin.site.register(Admin_Page, PostModelAdmin)
 
 
 
-admin.site.register(Admin_Page)
+# admin.site.register(Admin_Page)
