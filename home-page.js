689cbd84a7839df94ebb3fe9edd1d20bbff18621
shreyaa1 <shuklashreya801@gmail.com>
2018-09-21 01:35:53 +0530

    Added a class for closing

--- home-page.js
+++ home-page.js
@@ -2,3 +2,7 @@ function openForm(){
   var addClass = document.getElementsByClassName('form-wrapper')[0];
   addClass.classList.add("show-form");
 }
+function closeForm(){
+  var addCloseClass = document.getElementsByClassName('form-wrapper')[0];
+  addCloseClass.classList.remove("show-form");
+}
