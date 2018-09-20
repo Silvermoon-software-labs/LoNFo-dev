77f1d40e886c2ba61994853d36412cb47c7751e3
mephi007 <roysumit949@gmail.com>
2018-09-20 20:03:42 +0530

    created a page in which all the entries made into database can be seen, used jinja concept so let it be one entry or hundred, all the entries will come down in one single page

--- lnf/src/lnf/views.py
+++ lnf/src/lnf/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import LostNFound
 
 def Lnf_Detail(requests):
-	return render(requests, "Lnf_Detail.html", {})
+    queryset = LostNFound.objects.all()
+    details = {
+        "object_list" : queryset
+    }
+    return render(requests, "Lnf_Detail.html", details)
