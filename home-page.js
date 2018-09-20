a758daeff1ae79cfd0f0da799120504e40ca5c69
shreyaa1 <shuklashreya801@gmail.com>
2018-09-21 01:55:45 +0530

    added some classes based on condations need to refactor

--- home-page.js
+++ home-page.js
@@ -1,8 +1,15 @@
 function openForm(){
   var addClass = document.getElementsByClassName('form-wrapper')[0];
+  var addRemoveClass = document.getElementsByClassName('close-btn')[0];
+   var addClassforRemovingBtn = document.getElementsByClassName('btn-success')[0];
   addClass.classList.add("show-form");
+  addRemoveClass.classList.add("show-close-button")
+   addClassforRemovingBtn.classList.add("remove-sign-up")
+
 }
 function closeForm(){
   var addCloseClass = document.getElementsByClassName('form-wrapper')[0];
+ var remove = document.getElementsByClassName('btn-success')[0];
+ remove.classList.remove("remove-sign-up")
   addCloseClass.classList.remove("show-form");
 }
