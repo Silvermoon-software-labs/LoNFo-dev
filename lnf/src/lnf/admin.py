5cd65f916ccf211fa0ae5d412a72fdbaa4d0ff04
ujjwalcrypto <ujjwalsingh15@gmail.com>
2018-09-20 17:12:43 +0530

    `Working for tag and rearranged the code`

--- lnf/src/lnf/admin.py
+++ lnf/src/lnf/admin.py
@@ -1,20 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
-from lnf.models import Admin_Page
-
-class PostModelAdmin(admin.ModelAdmin):
-	list_display = ['Name', 'Contact', 'Lost_Found_Product', 'Small_Description','Tags','Location','Date_and_Time']
-	list_filter = ['Name', 'Contact', 'Lost_Found_Product', 'Small_Description','Tags','Location','Date_and_Time']
-	search_fields = ['Name', 'Contact', 'Lost_Found_Product', 'Small_Description','Tags','Location','Date_and_Time']
+from lnf.models import LostNFound
 
 
-	class Meta:
-		model = Admin_Page
+class LnfModelAdmin(admin.ModelAdmin):
+    list_display = ['Name', 'Email', 'Contact', 'Lost_Found_Product', 'Location', 'Date_and_Time']
+    list_filter = ['Name', 'Email', 'Contact', 'Lost_Found_Product', 'Small_Description','Location','Date_and_Time']
+    search_fields = ['Name', 'Email', 'Contact', 'Lost_Found_Product', 'Small_Description','Location','Date_and_Time']
+
+    # list_display = ['Name', 'tag_list']
+    # fieldsets = ((None, {'fields': ('tags',)}),)
+
+    # def get_queryset(self, request):
+    #     return super(lnfModelAdmin, self).get_queryset(request).prefetch_related('tags')
+
+    # def tag_list(self, obj):
+    #     return u", ".join(o.name for o in obj.tags.all())
+
+    class Meta:
+        model = LostNFound
 
 
-admin.site.register(Admin_Page, PostModelAdmin)
-
-
-
-# admin.site.register(Admin_Page)
+admin.site.register(LostNFound, LnfModelAdmin)
--- lnf/src/lnf/models.py
+++ lnf/src/lnf/models.py
@@ -1,22 +1,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
+# from taggit.managers import TaggableManager
 
 
 # Create your models here.
 
-class Admin_Page(models.Model):
-    Name = models.CharField(max_length = 30)
+class LostNFound(models.Model):
+    Name = models.CharField(max_length=30)
     Contact = models.IntegerField()
-    # Email = models.EmailField()
-    Lost_Found_Product = models.CharField(max_length = 10)
-    Small_Description = models.TextField(max_length = 120)
-    Tags = ArrayField(models.CharField(max_length=200), blank=True)
+    Email = models.EmailField()
+    Lost_Found_Product = models.CharField(max_length=10)
+    Small_Description = models.TextField(max_length=120)
+    # tags = TaggableManager()
     Location = models.TextField()
-    Date_and_Time = models.DateTimeField(auto_now = False , auto_now_add = True)
+    Date_and_Time = models.DateTimeField(auto_now=False, auto_now_add=True)
     
     def __str__(self):
-    	return self.Name
-    def __uicode__(self):
-    	return self.Name
+        return self.Name
+
+    def __unicode__(self):
+        return self.Name
