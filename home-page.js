3e339aac655d9f61ebd9d3d146ef81f822ffa1ef
shreyaa1 <shuklashreya801@gmail.com>
2018-09-24 23:49:12 +0530

    added style for sign up

--- home-page.js
+++ home-page.js
@@ -1,5 +1,5 @@
 function openForm(){
-  var addClass = document.getElementsByClassName('form-wrapper')[0];
+  var addClass = document.getElementsByClassName('navbar-form')[0];
   var addRemoveClass = document.getElementsByClassName('close-btn')[0];
    var addClassforRemovingBtn = document.getElementsByClassName('btn-success')[0];
   addClass.classList.add("show-form");
@@ -8,8 +8,8 @@ function openForm(){
 
 }
 function closeForm(){
-  var addCloseClass = document.getElementsByClassName('form-wrapper')[0];
- var remove = document.getElementsByClassName('btn-success')[0];
+  var addCloseClass = document.getElementsByClassName('navbar-form')[0];
+ var remove = document.getElementsByClassName('show-form')[0];
  remove.classList.remove("remove-sign-up")
   addCloseClass.classList.remove("show-form");
 }
