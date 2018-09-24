35fcc938969d2e82deec83822e8f94dafe629ba7
mephi007 <roysumit949@gmail.com>
2018-09-25 01:34:09 +0530

    sucessfully Search option is working for a given Name

--- lnf/src/lnf/views.py
+++ lnf/src/lnf/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.db.models import Q
 # Create your views here.
 from .models import LostNFound
 
 
 def dummy(requests):
+
     queryset = LostNFound.objects.all()
+    query = requests.GET.get("q")
+    if query:
+        queryset = queryset.filter(Q(Name__icontains=query))
+
     details = {
         "object_list": queryset
     }
+        
     return render(requests, "Lnf_Detail.html", details)
 
 
