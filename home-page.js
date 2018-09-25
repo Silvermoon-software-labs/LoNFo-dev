696e5485672caa395046e2bf43c7debc9ed2743e
shreyaa1 <shuklashreya801@gmail.com>
2018-09-26 00:58:58 +0530

    added style for navbar

--- home-page.js
+++ home-page.js
@@ -2,16 +2,20 @@ function openForm(){
   var addClass = document.getElementsByClassName('navbar-form')[0];
   var addRemoveClass = document.getElementsByClassName('close-btn')[0];
    var addClassforRemovingBtn = document.getElementsByClassName('btn-success')[0];
+   var hideMenu = document.getElementsByClassName('menu')[0];
   addClass.classList.add("show-form");
   addRemoveClass.classList.add("show-close-button")
    addClassforRemovingBtn.classList.add("remove-sign-up")
+   hideMenu.classList.add("hide-menu"); 
 
 }
 function closeForm(){
   var addCloseClass = document.getElementsByClassName('navbar-form')[0];
  var remove = document.getElementsByClassName('show-form')[0];
+ var showMenu = document.getElementsByClassName('menu')[0];
  remove.classList.remove("remove-sign-up")
   addCloseClass.classList.remove("show-form");
+  showMenu.classList.remove('hide-menu');
 }
 
 window.onscroll = function(){
